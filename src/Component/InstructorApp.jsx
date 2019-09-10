8913f6e6e28885229eaa368e2b21f15856af1659
javi <javi.perez.pino@gmail.com>
2019-09-10 21:05:27 +0200

    commit

---
+++ src/Component/InstructorApp.jsx
@@ -0,0 +1,28 @@
+import React, { Component } from 'react';
+import ListCoursesComponent from './ListCoursesComponent';
+import CourseComponent from './CourseComponent';
+
+import { Route, Switch, BrowserRouter as Router } from 'react-router-dom'
+
+
+
+
+class InstructorApp extends Component {
+    render() {
+        return (
+                <Router basename ="/frontend-jap-keep">
+                <>
+                    <h1>Instructor Application</h1>
+                    <Switch>
+                        
+                        <Route path="/" exact component={ListCoursesComponent} />
+                        <Route path="/courses" exact component={ListCoursesComponent} />
+                        <Route path="/courses/:id" component={CourseComponent} />
+                    </Switch>
+                </>
+                </Router>
+        )
+    }
+}
+
+export default InstructorApp
